cebd5ce00ad0a74c57a3e71e4be8b11e53f6f2e3
NKhalilov <nurmakhmad.khalilov7@gmail.com>
2021-04-19 10:44:13 +0500

    swapping forms, some minor changes in design and font

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
 $(".calendar").hide();
-$(".login").hide();
-// $("form").click(".btn-sign", function (e) {
-//     e.preventDefault(); //To not refresh the page
-//     $("#reg-form").remove(); //Remove the form
-//     $(".calendar").show();
-// });
-// $("a").click(".href", function (ev) {
-//     ev.preventDefault();
-//     $("#reg-form").remove();
-//     $(".login").show();
-    
-// });
-
-
+$("#reg-form").hide();
     $(document).ready(function(){
       $("form a").click(function (ev) {
           ev.preventDefault();
-          if ($(this).attr("id") == "signing") {
-              $("#reg-form").hide();
-          $(".calendar").show();
-      }
-          if ($(this).attr("id") == "logining") {
-              $("#reg-form").hide();
-          $(".login").show();
+    //       if ($(this).attr("id") == "signing") {
+    //         $("#reg-form").hide();
+    //       $(".calendar").show();
+    //   }
+        if ($(this).attr("id") == "signup") {
+            $("#login-form").hide();
+            $("#reg-form").show();
+        }
+        if ($(this).attr("id") == "logining") {
+            $("#reg-form").hide();
+            $("#login-form").show();
           }
-          if ($(this).attr("id") == "signup") {
-              $("#login-form").remove();
-              $("#reg-form").show();
-          }
+        
     });
 });
